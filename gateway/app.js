080028cf3df28e879c7c2744eca9c478191bb15f
Susanta Ray <susantaray786@gmail.com>
2025-01-09 15:29:26 +0530

    created a gateway server for management of user, captain and ride servers. express-http-proxy to redirect gateway server to their respective endpoints

---
+++ gateway/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import expressProxy from 'express-http-proxy';
+
+
+const app = express();
+
+
+app.use('/user', expressProxy('http://localhost:3001'));
+
+
+app.listen(3000, () => {
+  console.log('Gateway listening on port 3000');
+});
