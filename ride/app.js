178db805fff59635cbca95a2e068bac7098bde0a
Susanta Ray <susantaray786@gmail.com>
2025-01-10 03:47:15 +0530

    setup done for amqplib, a library to perform asynchronous connection between all the servers i.e user, captain and ride ✔

--- ride/app.js
+++ ride/app.js
@@ -6,14 +6,13 @@ import cookieParser from "cookie-parser";
 import connect from "./db/db.js";
 connect();
 import rideRouter from "./routes/ride.routes.js";
-
+import { connect as connectRabbit } from "./service/rabbit.js";
+connectRabbit();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.use("/", rideRouter);
-
 
 export default app;
---
+++ ride/service/rabbit.js
@@ -0,0 +1,28 @@
+import amqp from "amqplib";
+
+const RABBITMQ_URL = process.env.RABBIT_URL;
+
+let connection, channel;
+
+const connect = async () => {
+  connection = await amqp.connect(RABBITMQ_URL);
+  channel = await connection.createChannel();
+  console.log("Connected to RabbitMQ");
+};
+
+const subscribeToQueue = async (queueName, callback) => {
+  if (!channel) await connect();
+  await channel.assertQueue(queueName);
+  channel.consume(queueName, (message) => {
+    callback(message.content.toString());
+    channel.ack(message);
+  });
+};
+
+const publishToQueue = async (queueName, data) => {
+  if (!channel) await connect();
+  await channel.assertQueue(queueName);
+  channel.sendToQueue(queueName, Buffer.from(data));
+};
+
+export { subscribeToQueue, publishToQueue, connect };
